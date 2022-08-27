1d43b75bc33c01f4a2b8954958d1054060c056d2
Bastian Pérez <didier.bpp@gmail.com>
2022-08-27 02:33:34 -0400

    i advance in skill sectioon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 
-import { LegacyRef, RefObject, useRef } from 'react'
 import './assets/scss/styles.scss'
 import { NavBar } from './modules/layout/NavBar'
+import reactLogo from './assets/icons/react.svg'
+import vueLogo from './assets/icons/vue.svg'
+import typeScriptLogo from './assets/icons/typescript.svg'
 function App() {
   
   return (
@@ -20,6 +22,38 @@ function App() {
             </section>
             <section id="about">
               <h2>{'<AboutMe />'}</h2>
+              <div className="about__content">
+
+                <div className="about__text">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus veritatis fugit aut atque molestiae deleniti.</p>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, illum ullam! Soluta cumque deleniti tempora nesciunt nulla totam repudiandae eaque.</p>
+                </div>
+                {/* TODO: TRANSFORM ON A COMPONENT */}
+                <div className="about__skills">
+                    <ul>
+                      <li>
+                          <img src={reactLogo} alt="React logo" />
+                          <span>React</span>
+                       
+                      </li>
+                      <li>
+                        <img src={vueLogo} alt="Vue logo" />
+                        <span>Vue</span>
+                      </li>
+                      <li>
+                        <img src={typeScriptLogo} alt="Vue logo" />
+                        <span>TypeScript</span>
+                      </li>
+                      <li>Javascript</li>
+                      <li>HTML</li>
+                      <li>CSS</li>
+                    </ul>
+                </div>
+                 <div className="about__picture">
+
+                </div>
+                
+              </div>
             </section>
           </main>
       </div>
