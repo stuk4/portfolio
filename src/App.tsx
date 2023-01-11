8ba1c0b8f4e01532f6631dc3df6e8dc177adec1e
Bastian Pérez <didier.bpp@gmail.com>
2023-01-11 00:07:14 -0300

    Agrego social media

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,38 @@ import { Experience } from './modules/experience/Experience'
 import { Home } from './modules/home/Home'
 import { NavBar } from './modules/layout/NavBar'
 import { Projects } from './modules/projects/Projects'
+import { IconLinkedIn } from './modules/common/icons-svg/IconLinkedIn';
+import { IconGitHub } from './modules/common/icons-svg/IconGitHub';
 
 function App() {
-  
+
   return (
     <div className="layout">
-      <NavBar  />
-      <div id='content'>
-          <main>
-            <Home />
-            <About />
-            <Experience />
-            <Projects />
-            <Contact />
-          </main>
+      <NavBar />
+      <div className="social-media">
+        <ul>
+          <li>    
+              <a href="#">
+              <IconLinkedIn />
+              </a>
+          </li>
+          <li>    
+              <a href="#">
+              <IconGitHub />
+              </a>
+          </li>
+        </ul>
       </div>
+      <div id='content'>
+        <main>
+          <Home />
+          <About />
+          <Experience />
+          <Projects />
+          <Contact />
+        </main>
+      </div>
+
     </div>
   )
 }
